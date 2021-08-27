433c9feba05fdffd815738d6cc6ec6558942bf52
John Do <jdo697@gmail.com>
2021-08-27 08:02:31 -0700

    updated endpoint

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const staticMiddleware = require('./static-middleware');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
-  cors: { origin: `http://localhost:${process.env.DEV_SERVER_PORT}` }
+  cors: { origin: `https://group-chat-app1.herokuapp.com/` }
 });
 const cors = require('cors');
 const router = require('./routes');
